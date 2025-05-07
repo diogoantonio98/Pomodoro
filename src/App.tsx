9c7ade3c55f3a545a1ed9750fb3bc602c6e3e5e0
diogoWork <diogo98antonio@gmail.com>
2025-05-07 16:55:52 -0300

    trabalho

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+import { Heading } from './components/Heading';
+
+import './styles/theme.css';
+import './styles/global.css';
+
+
 export function App() {
 
     return (
         <>
-            <h1>Pomodoro</h1>
+            <Heading />
             <p>Lorem ipsum dolor sit amet, consectetur
                 adipisicing elit. Sit placeat ducimus ad
                 deserunt aperiam, assumenda nemo sapiente,
