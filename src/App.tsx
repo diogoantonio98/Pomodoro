b0d98cab02fc423c4380d86cdcf2c979e7bf790d
diogo <diogo98antonio@gmail.com>
2025-05-08 21:52:05 -0300

    didiWork

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
 import { Heading } from './components/Heading';
+import { TimerIcon } from 'lucide-react';
+import { Container } from './components/Conatiner';
+import { Logo } from './components/Logo';
 
 import './styles/theme.css';
 import './styles/global.css';
-import { TimerIcon } from 'lucide-react';
+
 
 
 export function App() {
 
     return (
         <>
-            <Heading>
-                Olá mund 1
-                <button>
-                    <TimerIcon />
-                </button>
-            </Heading>
-            <p>Lorem ipsum dolor sit amet, consectetur
-                adipisicing elit. Sit placeat ducimus ad
-                deserunt aperiam, assumenda nemo sapiente,
-                eligendi architecto nesciunt minima quasi
-                ipsam perspiciatis in quisquam consequatur
-                quibusdam quae dolore.</p>
+            <Container>
+                <Logo />
+            </Container>
+
+            <Container>
+                <Heading>Menu</Heading>
+            </Container>
         </>
     );
 }
--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -7,7 +7,7 @@ type HeadingProps = {
 export function Heading({ children }: HeadingProps) {
     return (
         <>
-            <h1 className={styles.Heading}>{children}</h1>
+            <h1 className={styles.heading}>{children}</h1>
         </>
     );
 }
