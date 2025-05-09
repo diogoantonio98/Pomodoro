7e25e70d8f31e2c90a5b3de2a3587abdaed078f7
diogoWork <diogo98antonio@gmail.com>
2025-05-09 16:55:33 -0300

    didiwork

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { Logo } from './components/Logo';
 
 import './styles/theme.css';
 import './styles/global.css';
+import { Menu } from './components/Menu';
 
 
 
@@ -17,7 +18,7 @@ export function App() {
             </Container>
 
             <Container>
-                <Heading>Menu</Heading>
+                <Menu />
             </Container>
         </>
     );
