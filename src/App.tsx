5e2e8d382bc9251e94757d650b085e09b61eeb09
diogo <diogo98antonio@gmail.com>
2025-05-11 22:04:52 -0300

    didiHome

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { Heading } from './components/Heading';
 import { TimerIcon } from 'lucide-react';
-import { Container } from './components/Conatiner';
+import { Container } from './components/Container';
 import { Logo } from './components/Logo';
 
 import './styles/theme.css';
 import './styles/global.css';
 import { Menu } from './components/Menu';
+import { CountDown } from './components/CountDown';
+import { DefaultInput } from './components/DefaultInput';
+import { Cycles } from './components/Cycles';
 
 
 
@@ -19,6 +22,35 @@ export function App() {
 
             <Container>
                 <Menu />
+            </Container>
+
+            <Container>
+                <CountDown />
+            </Container>
+
+            <Container>
+                <form className='form' action=''>
+                    <div className='formRow'>
+                        <DefaultInput
+                            labelText='task'
+                            id='meuInput'
+                            type='text'
+                            placeholder='Digite algo'
+                        />
+                    </div>
+
+                    <div className='formRow'>
+                        <p>Lorem ipsum dolor sit amet.</p>
+                    </div>
+
+                    <div className='formRow'>
+                        <Cycles />
+                    </div>
+
+                    <div className='formRow'>
+                        <button>Enviar</button>
+                    </div>
+                </form>
             </Container>
         </>
     );
