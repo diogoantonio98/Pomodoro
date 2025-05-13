14378fafcb7a8d3e559fc675067d9c8a8d9b5a5a
diogoWork <diogo98antonio@gmail.com>
2025-05-13 14:48:54 -0300

    didiWork

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,8 @@
-import { Heading } from './components/Heading';
-import { PlayCircleIcon, StopCircleIcon, TimerIcon } from 'lucide-react';
-import { Container } from './components/Container';
-import { Logo } from './components/Logo';
 
 import './styles/theme.css';
 import './styles/global.css';
-import { Menu } from './components/Menu';
-import { CountDown } from './components/CountDown';
-import { DefaultInput } from './components/DefaultInput';
-import { Cycles } from './components/Cycles';
-import { DefaultButton } from './components/DefaultButton';
-import { Footer } from './components/Footer';
+
+import { Home } from './pages/Home';
 
 
 
@@ -24,48 +16,5 @@ export function App() {
     // setNumero(prevState => prevState + 1);
 
 
-    return (
-        <>
-            <Container>
-                <Logo />
-            </Container>
-
-            <Container>
-                <Menu />
-            </Container>
-
-            <Container>
-                <CountDown />
-            </Container>
-
-            <Container>
-                <form className='form' action=''>
-                    <div className='formRow'>
-                        <DefaultInput
-                            labelText='task'
-                            id='meuInput'
-                            type='text'
-                            placeholder='Digite algo'
-                        />
-                    </div>
-
-                    <div className='formRow'>
-                        <p>Lorem ipsum dolor sit amet.</p>
-                    </div>
-
-                    <div className='formRow'>
-                        <Cycles />
-                    </div>
-
-                    <div className='formRow'>
-                        <DefaultButton icon={<PlayCircleIcon />} />
-                    </div>
-                </form>
-            </Container>
-
-            <Container>
-                <Footer />
-            </Container>
-        </>
-    );
+    return <Home />;
 }
--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,4 +1,4 @@
-import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from 'lucide-react';
+import { HistoryIcon, HouseIcon, MoonIcon, SettingsIcon, SunIcon } from 'lucide-react';
 import styles from './Menu.module.css';
 import { useEffect, useState } from 'react';
 
@@ -6,7 +6,16 @@ type AvailableThemes = 'dark' | 'light';
 
 export function Menu() {
 
-    const [theme, setTheme] = useState<AvailableThemes>('dark');
+    const [theme, setTheme] = useState<AvailableThemes>(() => {
+        const storageTheme =
+            (localStorage.getItem('theme') as AvailableThemes) || 'dark';
+        return storageTheme;
+    });
+
+    const nextThemeIcon = {
+        dark: <SunIcon />,
+        light: <MoonIcon />,
+    };
 
     function handleThemeChange(
         event: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
@@ -32,14 +41,11 @@ export function Menu() {
         console.log('Theme mudou', theme, Date.now());
         document.documentElement.setAttribute('data-theme', theme);
 
-        return () => {
-            console.log('Olha, este componente será atualizado');
-        };
+        localStorage.setItem('theme', theme);
     }, [theme]); // Executa apenas quando o valor de theme muda
 
     return (
         <nav className={styles.menu}>
-            <h1>{theme}</h1>
             <a
                 className={styles.menuLink}
                 href='#'
@@ -74,7 +80,7 @@ export function Menu() {
                 title='Mudar Tema'
                 onClick={handleThemeChange}
             >
-                <SunIcon />
+                {nextThemeIcon[theme]}
             </a>
         </nav>
     );
